f66bcba8b3a50048656e3f5b2ab8dce9ffcd669c
Abdul Rafay <abdulrafaybinrauf@gmail.com>
2024-11-20 19:31:06 +0500

    Initial-Code-1

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+class Flight {
+public:
+ 
+};
+
+class City {
+public:
+
+};
+
+class Preferences {
+public:
+
+};
+
+class FlightGraph {
+private:
+};
+
+class RouteFinder {
+public:
+};
+
+// LayoverManager class
+class LayoverManager {
+public:
+};
+
+// UserInterface class
+class UserInterface {
+private:
+};
+
+int main() {
+}
