26f23cdd72046f5e399ff7a41db2f9d9d079ec4a
SYED MUHAMMAD SAAD BUKHARI <142715489+saadbukhari00@users.noreply.github.com>
2024-11-25 17:43:48 +0500

    Update route.h

--- Classes/route.h
+++ Classes/route.h
@@ -1,33 +1,32 @@
 #ifndef ROUTE_H
 #define ROUTE_H
 
-#include <iostream>
+#include "FlightGraph.h"
 #include <climits>
-#include "filehandling.h"
-#include "heap.h"
-#include "queue.h"
-#include "stack.h"
+#include <cstring>
+#include <iostream>
 
-class Route {
+class Route 
+{
 private:
-    struct Node {
-        int cityIndex; // Index of the city
-        int cost;      // Cost to reach this city
-        int time;      // Time to reach this city
+    FlightGraph& flightGraph;
 
-        Node(int cityIndex, int cost, int time)
-            : cityIndex(cityIndex), cost(cost), time(time) {}
-    };
+    void initializeArrays(int* distances, int* previous, bool* visited, int size);
 
-    int findCityIndex(const  string& cityName,  string cities[], int cityCount);
-    void initializeGraph(int graph[100][100], int size);
+    int findMinDistanceIndex(int* distances, bool* visited, int size);
 
 public:
-    void findShortestRoute(const  string& origin, const  string& destination, int cityCount,  string cities[], int graph[100][100]);
-    void findCheapestRoute(const  string& origin, const  string& destination, int cityCount,  string cities[], int graph[100][100]);
-    void listFlightsInRange(int minCost, int maxCost, Flight flights[], int flightCount);
-    void displayDirectFlights(const  string& origin, const  string& destination, Flight flights[], int flightCount);
-    void displayIndirectFlights(const  string& origin, const  string& destination, Flight flights[], int flightCount);
+    Route(FlightGraph& graph);
+
+    void findShortestRoute(const char* startCity, const char* endCity);
+
+    void findCheapestRoute(const char* startCity, const char* endCity);
+
+    void listShortestAndCheapest(const char* startCity, const char* endCity);
+
+    void displayFlights(const char* city);
+
+    void findFlightsWithinRange(const char* startCity, int minRange, int maxRange);
 };
 
-#endif
+#endif // ROUTE_H
