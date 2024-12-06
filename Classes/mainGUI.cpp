10f4ddb60f87e5ef5ee675bdee5e2664565ddbab
Huzaifa Malik <huzaifa3879@gmail.com>
2024-12-07 00:19:42 +0500

    Added MainGUI Class

---
+++ Classes/mainGUI.cpp
@@ -0,0 +1,40 @@
+#include "mainGUI.h"
+
+
+
+void MainGUI::draw() 
+    {
+        mainWindow.clear(sf::Color::White);
+
+        // Draw flight graph
+        sf::Texture mapTexture;
+        mapTexture.loadFromFile("Assets/world_map.png");
+        flightGraph.displayOnMap(mainWindow, mapTexture);
+
+        // Draw Book Flight button
+        mainWindow.draw(bookFlightButton);
+        mainWindow.draw(buttonText);
+        mainWindow.display();
+    }
+
+    bool MainGUI::handleEvents() {
+        sf::Event event;
+        while (mainWindow.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                mainWindow.close();
+                return false;
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
+                if (bookFlightButton.getGlobalBounds().contains(mousePos)) {
+                    return true; // Switch to booking window
+                }
+            }
+        }
+        return false;
+    }
+
+ void MainGUI::addAirplane(const Airplane& newAirplane)
+    {
+        airplane = newAirplane; // Store the airplane passed from the search
+    }
---
+++ Classes/mainGUI.h
@@ -0,0 +1,41 @@
+#ifndef MAINGUI_H
+#define MAINGUI_H
+
+
+#include"main.h"
+#include"FlightGraph.h"
+
+class MainGUI {
+private:
+    sf::RenderWindow& mainWindow;
+    sf::Font font;
+    sf::RectangleShape bookFlightButton;
+    sf::Text buttonText;
+    FlightGraph& flightGraph;
+    Airplane airplane; // Adding airplane as a member variable
+
+public:
+    MainGUI(sf::RenderWindow& window, FlightGraph& graph)
+        : mainWindow(window), flightGraph(graph), airplane() {
+        font.loadFromFile("Assets/Aller_Bd.ttf");
+
+        // Configure the Book Flight button
+        bookFlightButton.setSize(sf::Vector2f(150, 50));
+        bookFlightButton.setPosition(10, 10);
+        bookFlightButton.setFillColor(sf::Color::Green);
+
+        buttonText.setFont(font);
+        buttonText.setCharacterSize(20);
+        buttonText.setFillColor(sf::Color::Black);
+        buttonText.setString("Book Flight");
+        buttonText.setPosition(25, 20);
+    }
+
+    bool handleEvents();
+
+    void addAirplane(const Airplane& newAirplane);
+
+    void draw();
+};
+
+#endif
