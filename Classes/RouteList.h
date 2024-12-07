874d02493de39eab9a9bf9535774310df4fe7795
SYED MUHAMMAD SAAD BUKHARI <142715489+saadbukhari00@users.noreply.github.com>
2024-12-07 21:07:53 +0500

    update

---
+++ Classes/RouteList.h
@@ -0,0 +1,32 @@
+#ifndef ROUTELIST_H
+#define ROUTELIST_H
+
+#include "main.h"
+#include "list.h"
+
+
+struct FlightRoute {
+    LinkedList legs;
+};
+
+class RouteList {
+public:
+    struct RouteNode {
+        FlightRoute route;
+        RouteNode* next;
+    };
+    
+    RouteList() : head(NULL) {}
+
+    void insertRoute(FlightRoute& newRoute);
+    void Display();
+    void clear();
+    int countRoutes();
+    RouteNode* getRouteByIndex(int idx);
+    RouteNode* getHead() { return head; }
+    bool hasRoutes() { return head != NULL; }
+private:
+    RouteNode* head;
+};
+
+#endif
